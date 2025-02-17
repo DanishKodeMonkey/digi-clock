3e590137afde56487bebaba186c490a36214fc46
danishkodemonkey <humberjini@hotmail.com>
2025-02-17 14:18:34 +0100

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,36 @@
+# Python PyQt5 Digital Clock
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt5.QtCore import QTimer, QTime, Qt
+
+
+class DigitalClock(
+    QWidget
+):  # Widget inheritence from PyQt5, initialise components and elements
+    def __init__(self):  # Custom constructor
+        super().__init__()  # Inherit QWidget constructor
+        self.time_label = QLabel("12:00:00", self)
+
+        self.timer = QTimer(self)
+
+        self.initUI()
+
+    def initUI(self):  # Customize components and elements.
+        self.setWindowTitle("Digital Clock")
+        self.setGeometry(1600, 1000, 400, 100)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.time_label)
+
+        self.setLayout(vbox)
+
+        self.time_label.setAlignment(Qt.AlignCenter)
+        self.time_label.setStyleSheet("font-size: 60px;")
+
+
+if __name__ == "__main__":  # Initialize program if launched standalone
+    app = QApplication(sys.argv)
+    clock = DigitalClock()
+    clock.show()
+    sys.exit(app.exec_())  # Initialise event loop of app and handle events within it

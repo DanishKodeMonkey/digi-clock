8491a8c6c0d785e2e3187f633c67930f628a10a5
danishkodemonkey <humberjini@hotmail.com>
2025-02-17 14:44:10 +0100

    initial commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import sys
+from PyQt5.QtWidgets import (
+    QApplication,
+    QWidget,
+    QLabel,
+    QPushButton,
+    QVBoxLayout,
+    QHBoxLayout,
+)
+from PyQt5.QtCore import QTimer, QTime, Qt
